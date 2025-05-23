e0c0aeabe1fc5c9d392432c371c850423a946996
Mimi L <lim.mimic@gmail.com>
2025-05-23 14:26:16 +0200

    tour-info update

--- src/pages/tour-info.jsx
+++ src/pages/tour-info.jsx
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import { TourContext } from '../context/tour-context';
 
 export const TourInfo = ({props, showTourInfo, closeTourInfo}) => {
-  const {cartItems, addToCart} = useContext(TourContext);
+  const [ pax, setPax ] = useState(1);
+  const {cartItems, addToCart, removeFromCart} = useContext(TourContext);
   const {id, tourName, img, description, duration, languages, meetingPoint, price} = props.data;
-  
+
   const addTourToCart = () => {
     addToCart(id);
     closeTourInfo();
@@ -13,9 +14,23 @@ export const TourInfo = ({props, showTourInfo, closeTourInfo}) => {
   if (!showTourInfo) {return null}
   return (
     <div className='tourInfo'>
-      <h1>tour-info</h1>
-      <button onClick={closeTourInfo}>Close</button>
-      <button onClick={addTourToCart}>Add To Cart</button>
+      <div className='detail'>
+        <h1>{tourName}</h1>
+        <p>{description}</p>
+        <p>{meetingPoint}</p>
+      </div>
+      <div className='countHandler'>
+        <p>Number of People: </p>
+        <div><button onClick={()=>setPax(pax-1)}> - </button> {pax} <button onClick={()=>setPax(pax+1)}> + </button></div>
+        
+      </div>
+      <div className='dateHandler'>
+        <label>Date: </label>
+      </div>
+      <div className='bttns'>
+        <button onClick={closeTourInfo}>Close</button>
+        <button onClick={addTourToCart}>Add To Cart</button>
+      </div>
     </div>
   )
 }

02f1c59cec67bdedd54a28c6f2538584fa34a5c4
Mimi L <lim.mimic@gmail.com>
2025-05-23 09:24:25 +0200

    update tour page

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TOURS } from '../tours'
+import { TourContext } from '../context/tour-context'
 
 export const Cart = () => {
+
+  const { cartItem } = useState(TourContext);
   return (
     <div className='cart'>
       <h1>Cart page</h1>

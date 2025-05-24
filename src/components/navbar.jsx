d3e5b8f936f0e0ad8e9ebb4649f0192906133cf7
Mimi L <lim.mimic@gmail.com>
2025-05-24 17:37:33 +0200

    scroll top button update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { ShoppingBagIcon } from '@phosphor-icons/react'
+import { ArrowCircleUpIcon, ShoppingBagIcon } from '@phosphor-icons/react'
 import '../styles/navbar.css'
 
 export const Navbar = () => {
+
+  const [ showBttn, setShowBttn ] = useState(false);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth'});
+  };
+
+  useEffect(() => {
+    const handleScrollBttnVisibility = () => {
+      window.pageYOffset > 300 ? setShowBttn(true) : setShowBttn(false);
+    };
+
+    window.addEventListener('scroll', handleScrollBttnVisibility);
+
+    return () => {
+     window.addEventListener('scroll', handleScrollBttnVisibility);
+    };
+  });
+
   return (
     <div className='navbar'>
       <div className='links'>
@@ -12,6 +31,11 @@ export const Navbar = () => {
         <Link to='/contact'> Contact </Link>
         <Link to='/cart'><ShoppingBagIcon size={28} /></Link>
       </div>
+      {showBttn && (
+        <div className='scrollToTop'>
+          <button className='scrollBttn' onClick={handleScrollToTop}><ArrowCircleUpIcon size={50} /></button>
+        </div>
+      )}
     </div>
   )
 }

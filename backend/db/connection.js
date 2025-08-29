3c9befe2b01d047a1620a8b6db827c2fd10d13ac
Mimi L <lim.mimic@gmail.com>
2025-08-29 15:10:56 +0200

    edit: mongoose uri

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI).then(()=>console.log("Successfully connected to MongoDB")).catch((error)=>console.log("Couldn't connect to MongoDB"));
+mongoose.connect(process.env.MONGO_URI).then(()=>console.log("Successfully connected to MongoDB")).catch((error)=>console.log("Couldn't connect to MongoDB: ", error));
